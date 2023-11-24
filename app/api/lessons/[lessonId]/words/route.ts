1bae81b6ff4598329722ea47657219c97700a8e5
Rob Sliwa <robjsliwa@gmail.com>
2023-11-24 14:55:15 -0500

    Updated APIs to server as ACTIONS for custom GPT.

--- app/api/lessons/[lessonId]/words/route.ts
+++ app/api/lessons/[lessonId]/words/route.ts
@@ -1,35 +1,73 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { kv } from "@vercel/kv";
+import { isValidApiKey } from '@/utils';
 
 
-export function GET(request: NextRequest, { params }: { params: { lessonId: string } }) {
+export async function GET(request: NextRequest, { params }: { params: { lessonId: string } }) {
+  if (!isValidApiKey(request)) {
+    return NextResponse.json(
+      {
+        message: 'Invalid API key',
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+  const lessonId = params.lessonId;
+  const wordsString: string | null = await kv.get(lessonId);
+
+  if (!wordsString) {
+    return NextResponse.json(
+      {
+        word: '',
+      },
+      {
+        status: 201,
+      },
+    );
+  }
+
+  const words = wordsString.split(', ');
+  const randomWord = words[Math.floor(Math.random() * words.length)];
+
   return NextResponse.json(
     {
-      verb: 'GET',
-      message: `lessonId: ${params.lessonId}`,
-      body: request.body,
-      path: request.nextUrl.pathname,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
+      word: randomWord,
     },
     {
-      status: 200,
+      status: 201,
     },
   );
 }
 
-export function POST(request: NextRequest) {
+export async function POST(request: NextRequest, { params }: { params: { lessonId: string } }) {
+  if (!isValidApiKey(request)) {
+    return NextResponse.json(
+      {
+        message: 'Invalid API key',
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+  const requestBody = await request.json();
+  const lessonId = params.lessonId;
+  const newWords = requestBody.words;
+
+  const existingWords = await kv.get(lessonId) || '';
+  const updatedWords = existingWords + (existingWords ? ', ' : '') + newWords.join(', ');
+
+  await kv.set(lessonId, updatedWords, { ex: 43200 });
+
   return NextResponse.json(
     {
-      verb: 'POST',
-      message: 'Hello from the API',
-      body: request.body,
-      path: request.nextUrl.pathname,
-      query: request.nextUrl.search,
-      cookies: request.cookies.getAll(),
+      message: 'Words added successfully',
     },
     {
-      status: 200,
+      status: 201,
     },
   );
 }
